1879e953ad86a8798c7585393bd075142668b073
Alper Sunar <alper.sunar@autodesk.com>
2020-07-12 08:27:01 -0500

    Add baddata test

--- chapter6/test/HelloWorld.Test/BulkEventLambdaFunctionalTests.cs
+++ chapter6/test/HelloWorld.Test/BulkEventLambdaFunctionalTests.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.Lambda.S3Events;
@@ -9,19 +9,22 @@ using Amazon.S3.Model;
 using Amazon.S3.Util;
 using Amazon.SimpleNotificationService;
 using FakeItEasy;
+using FluentAssertions;
 using Xunit;
 
 namespace BulkEvents.Tests
 {
     public class BulkEventLambdaFunctionalTests
     {
-        [Fact]
-        public void ShouldHandleBulkWeatherEvents()
-        {
-            var fakeSns = A.Fake<AmazonSimpleNotificationServiceClient>();
-            var fakeS3 = A.Fake<AmazonS3Client>();
+        private readonly S3Event _fakeS3Event;
+        private readonly AmazonSimpleNotificationServiceClient _fakeSns;
+        private readonly AmazonS3Client _fakeS3;
+        private readonly GetObjectRequest _s3GetObjectRequest;
+        private readonly string _snsTopic;
 
-            var s3Event = new S3Event()
+        public BulkEventLambdaFunctionalTests()
+        {
+            _fakeS3Event = new S3Event()
             {
                 Records = new List<S3EventNotification.S3EventNotificationRecord>
                 {
@@ -41,30 +44,65 @@ namespace BulkEvents.Tests
                     }
                 }
             };
-
-
-            var s3GetObjectRequest = new GetObjectRequest()
+            _fakeSns = A.Fake<AmazonSimpleNotificationServiceClient>();
+            _fakeS3 = A.Fake<AmazonS3Client>();
+            _s3GetObjectRequest = new GetObjectRequest()
             {
                 BucketName = "fakeBucket",
                 Key = "fakeKey"
             };
+            _snsTopic = "test-topic";
+        }
 
-            var s3ObjectResponse = new GetObjectResponse
+        [Fact]
+        public void ShouldPublishForEachWeatherEvent()
+        {
+
+            // Create mock object response from sampledata.json
+            var s3ResponseWithSampleData = new GetObjectResponse
             {
                 ResponseStream = File.OpenRead("../../../../../sampledata.json")
             };
+
+            // Tell fake S3 to return to return the mock object response created above.
             A.CallTo(() => 
-                fakeS3.GetObjectAsync(A<GetObjectRequest>.That.Matches(x => x.BucketName == s3GetObjectRequest.BucketName && x.Key == s3GetObjectRequest.Key), A<CancellationToken>._))
-                .Returns(Task.FromResult(s3ObjectResponse));
+                _fakeS3.GetObjectAsync(A<GetObjectRequest>.That.Matches(x => x.BucketName == _s3GetObjectRequest.BucketName && x.Key == _s3GetObjectRequest.Key), A<CancellationToken>._))
+                .Returns(Task.FromResult(s3ResponseWithSampleData));
 
-            var topic = "test-topic";
 
-            var bulkEventsLambda = new BulkEventsLambda(fakeS3, fakeSns, topic);
-            bulkEventsLambda.S3EventHandler(s3Event);
+            var bulkEventsLambda = new BulkEventsLambda(_fakeS3, _fakeSns, _snsTopic);
+            bulkEventsLambda.S3EventHandler(_fakeS3Event);
 
+            // 3 events in sampledata.json => expect 3 sns publish calls.
             A.CallTo(() =>
-                    fakeSns.PublishAsync(topic, A<string>._, CancellationToken.None))
+                    _fakeSns.PublishAsync(_snsTopic, A<string>._, CancellationToken.None))
                 .MustHaveHappened(3, Times.Exactly);
+
+
+            var expectedMessage = "{\"LocationName\":\"New York, NY\",\"Temperature\":91,\"Timestamp\":1564428897,\"Longitude\":-73.99,\"Latitude\":40.7}" ;
+            A.CallTo(() =>
+                    _fakeSns.PublishAsync(_snsTopic, expectedMessage, CancellationToken.None))
+                .MustHaveHappenedOnceExactly();
+
+        }
+
+        [Fact]
+        public void ShouldThrowWithInvalidJson()
+        {
+            var s3ResponseWithBadData = new GetObjectResponse
+            {
+                ResponseStream = File.OpenRead("../../../../../baddata.json")
+            };
+
+            // Tell fake S3 to return to return the mock object response created above.
+            A.CallTo(() => 
+                    _fakeS3.GetObjectAsync(A<GetObjectRequest>.That.Matches(x => x.BucketName == _s3GetObjectRequest.BucketName && x.Key == _s3GetObjectRequest.Key), A<CancellationToken>._))
+                .Returns(Task.FromResult(s3ResponseWithBadData));
+
+            var bulkEventsLambda = new BulkEventsLambda(_fakeS3, _fakeSns, _snsTopic);
+            Action act = () => bulkEventsLambda.S3EventHandler(_fakeS3Event);
+
+            act.Should().Throw<System.Text.Json.JsonException>();
 
         }
     }
